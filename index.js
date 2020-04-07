8fd0edc22f26c33f39264959ed9ef55b23345aad
Akins <smo244@gmail.com>
2020-04-07 10:59:13 -0700

    removed duplicate files

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const bodyParser = require('body-parser')
 const app = express()
 const port  = 3000
 const db = require('./queries')
+const helmet = require('helmet')
+const compression = require('compression')
+const rateLimit = require ('express-rate-limit')
+const { body, check } = require('express-validator')
+
+// limit express requests
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, 
+    max: 5,
+})
+const postLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 1,
+})
+app.use(limiter)
+
+
+// add compression and HTTP header security
+app.use(compression())
+app.use(helmet())
 
 app.use(bodyParser.json())
 app.use(
@@ -21,6 +41,20 @@ app.listen(port, () => {
 
 app.get('/crops', db.getCrops)
 app.get('/crops/:id', db.getCropById)
-app.post('/crops', db.createCrop)
+app.post('/crops', 
+// [
+//     check('commonname')
+//       .not()
+//       .isEmpty()
+//       .isLength({ min: 2, max: 255 })
+//       .trim(),
+//     check('scientificname')
+//       .not()
+//       .isEmpty()
+//       .isLength({ min: 2, max: 255 })
+//       .trim(),
+// ],
+postLimiter, 
+db.createCrop)
 app.put('/crops/:id', db.updateCrop)
 app.delete('/crops/:id', db.deleteCrop)
